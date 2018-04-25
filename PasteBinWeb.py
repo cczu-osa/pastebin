fe9accdb5588953eb5a5ca20d4987136f5282b82
Weicheng <williamjiang97@gmail.com>
2018-04-25 13:05:28 +0800

    Revert "Update All files"
    This reverts commit 2102b052a14ed80d49c0dbe8b669f6f154521fb4.

--- PasteBinWeb.py
+++ PasteBinWeb.py
@@ -52,9 +52,8 @@ def num(stamp):
                 file_path = os.path.join(paste_path, file)
                 code_source = open(file_path).read()
                 language = os.path.splitext(file)[1][1:]
-                time_stamp = time.ctime(os.stat(file_path).st_ctime)
-                date = time_stamp
-                #date = time.strftime("%Y-%m-%d %H:%M:%S", time_stamp)
+                time_stamp = time.gmtime(os.path.getatime(file_path))
+                date = time.strftime("%Y-%m-%d %H:%M:%S", time_stamp)
                 lang = get_lexer_by_name(language)
                 formatter = HtmlFormatter(encoding='utf-8', style='emacs', linenos=True)
                 code = highlight(code_source, lang, formatter).decode("utf8").replace('highlighttable', 'pastetable', 1)
@@ -71,17 +70,15 @@ def all():
         for file in os.listdir(paste_path):
             file_path = os.path.join(paste_path, file)
             path = os.path.splitext(file)[0][0:]
-            time_stamp = time.ctime(os.stat(file_path).st_ctime)
-            #date = time_stamp
-            code_source = ''
-            f = open(file_path)
-            for i in range(10):
-                code_source = code_source + f.readline()
+            time_stamp = time.gmtime(os.path.getatime(file_path))
+            date = time.strftime("%Y-%m-%d %H:%M:%S", time_stamp)
+
+            code_source = open(file_path).read()
             language = os.path.splitext(file)[1][1:]
             lang = get_lexer_by_name(language)
             formatter = HtmlFormatter(encoding='utf-8', style='emacs', linenos=True)
             code = highlight(code_source, lang, formatter).decode("utf8").replace('highlighttable', 'pastetable', 1)
-            posts.append(dict(url=path, dat=time_stamp,content=code))
+            posts.append(dict(url=path, dat=date,content=code))
             #print(os.path.splitext(file)[0][1:])
             #list.append()
             #list.append()
