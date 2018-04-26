0bbd9e2b15a94f8bd072e7e666177352281f2428
Weicheng <williamjiang97@gmail.com>
2018-04-26 21:10:09 +0800

    Update
    Use gunicorn to run app to avoid problems of flask

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ MAINTAINER Weicheng Jiang "williamjiang97@gmail.com"
 ADD . /flask_compose
 WORKDIR /flask_compose
 RUN pip install --no-cache-dir -r requirements.txt
-CMD python PasteBinWeb.py
+CMD gunicorn -w 4 -b 8.8.8.8:80 PasteBinWeb:app
--- PasteBinWeb.py
+++ PasteBinWeb.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, send_file, redirect
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
-from jinja2 import Template
 import os
 import fnmatch
 import string
@@ -14,7 +13,7 @@ import shutil
 app = Flask(__name__)
 
 # 获取工作目录路径
-if(sys.path[0]==os.getcwd()):
+if (sys.path[0] == os.getcwd()):
     p = sys.path[0]
 else:
     p = sys.path[1]
@@ -22,8 +21,10 @@ paste_path = os.path.join(p, 'pastefile')
 try:
     os.mkdir(paste_path)
 except BaseException as e:
-    print(str(e))
+    print("粘贴文件夹已创建")
 error_file_path = os.path.join(p, 'static', 'error.html')
+
+
 # templates does not need absolute path.
 # index_file_path = os.path.join(p, 'templates', 'index.html')
 # pasted_file_path = os.path.join(p, 'templates', 'pasted.html')
@@ -62,7 +63,7 @@ def num(stamp):
                 language = os.path.splitext(file)[1][1:]
                 time_stamp = time.ctime(os.stat(file_path).st_ctime)
                 date = time_stamp
-                #date = time.strftime("%Y-%m-%d %H:%M:%S", time_stamp)
+                # date = time.strftime("%Y-%m-%d %H:%M:%S", time_stamp)
                 lang = get_lexer_by_name(language)
                 formatter = HtmlFormatter(encoding='utf-8', style='emacs', linenos=True)
                 code = highlight(code_source, lang, formatter).decode("utf8").replace('highlighttable', 'pastetable', 1)
@@ -71,21 +72,22 @@ def num(stamp):
     except IOError:
         return send_file(error_file_path)
 
+
 @app.route('/all')
 def all():
     try:
-        posts=[]
+        posts = []
         os.chdir(paste_path)
         files = filter(os.path.isfile, os.listdir(paste_path))
         files = [os.path.join(paste_path, f) for f in files]
         files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
         for file in files:
             file_path = os.path.join(paste_path, file)
-            #print(file_path+time.ctime(os.stat(file_path).st_ctime))
+            # print(file_path+time.ctime(os.stat(file_path).st_ctime))
             path = os.path.splitext(file)[0][0:]
             file_name = os.path.basename(file_path)
             time_stamp = time.ctime(os.stat(file_path).st_ctime)
-            #date = time_stamp
+            # date = time_stamp
             code_source = ''
             f = open(file_path)
             for i in range(10):
@@ -94,20 +96,22 @@ def all():
             lang = get_lexer_by_name(language)
             formatter = HtmlFormatter(encoding='utf-8', style='emacs', linenos=True)
             code = highlight(code_source, lang, formatter).decode("utf8").replace('highlighttable', 'pastetable', 1)
-            posts.append(dict(url=file_name, dat=time_stamp,content=code))
-            #print(os.path.splitext(file)[0][1:])
-            #list.append()
-            #list.append()
-        return render_template('all.html',data=posts)
+            posts.append(dict(url=file_name, dat=time_stamp, content=code))
+            # print(os.path.splitext(file)[0][1:])
+            # list.append()
+            # list.append()
+        return render_template('all.html', data=posts)
     except BaseException as e:
         print(str(e))
         return send_file(error_file_path)
+
 
 @app.route('/clean')
 def clean():
     shutil.rmtree(paste_path)
     os.mkdir(paste_path)
     return render_template('index.html', state='')
+
 
 # err no such file
 @app.errorhandler(404)
